67fb7f1f9cc63d583dffaeef548df229ca2f7bcd
SirDaigo <88176636+SirDaigo@users.noreply.github.com>
2026-02-04 00:57:03 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,20 +2,20 @@
 // Function to handle button click events
 function selectOption(option) {
     // Check which option was clicked
-    if (option === 'yes') {
+    if (option === 'si') {
         // Flash rainbow colors
         flashRainbowColors(function() {
             document.getElementById('question').style.display = 'none'; // Hide the question
             displayCatHeart(); // Display the cat-heart.gif
         });
     } else if (option === 'no') {
-        // Change text on the "No" button to "You sure?"
-        document.getElementById('no-button').innerText = 'You sure?'; 
-        // Increase font size of "Yes" button
-        var yesButton = document.getElementById('yes-button');
-        var currentFontSize = window.getComputedStyle(yesButton).getPropertyValue('font-size');
+        // Change text on the "No" button to "¿Segura?"
+        document.getElementById('no-button').innerText = '¿Segura?'; 
+        // Increase font size of "Si" button
+        var siButton = document.getElementById('si-button');
+        var currentFontSize = window.getComputedStyle(siButton).getPropertyValue('font-size');
         var newSize = parseFloat(currentFontSize) * 2; // Increase font size by  * 2px
-        yesButton.style.fontSize = newSize + 'px';
+        siButton.style.fontSize = newSize + 'px';
     } else {
         // If neither "Yes" nor "No" was clicked, show an alert message
         alert('Invalid option!');
